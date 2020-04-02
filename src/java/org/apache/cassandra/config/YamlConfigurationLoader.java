032fbd8cca1d7b5b96738bd96d9672864426d718
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2020-04-02 15:29:44 -0400

    Provide to the users the opportunity to specify units on their own in cassandra.yaml (CASSANDRA-15234)

--- src/java/org/apache/cassandra/config/YamlConfigurationLoader.java
+++ src/java/org/apache/cassandra/config/YamlConfigurationLoader.java
@@ -25,16 +25,15 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
-
 import org.apache.commons.lang3.SystemUtils;
 
 import org.slf4j.Logger;
@@ -60,7 +59,8 @@ public class YamlConfigurationLoader implements ConfigurationLoader
     /**
      * Inspect the classpath to find storage configuration file
      */
-    private static URL getStorageConfigURL() throws ConfigurationException
+    @VisibleForTesting
+    public static URL getStorageConfigURL() throws ConfigurationException
     {
         String configUrl = System.getProperty("cassandra.config");
         if (configUrl == null)
@@ -126,9 +126,15 @@ public class YamlConfigurationLoader implements ConfigurationLoader
             Yaml yaml = new Yaml(constructor);
             Config result = loadConfig(yaml, configBytes);
             propertiesChecker.check();
+            //CASSANDRA-15234 - parse the properties for units and convert any values if needed
+            if(!isOldYAML)
+            {
+                Config.parseUnits(result, url);
+            }
+
             return result;
         }
-        catch (YAMLException e)
+        catch (YAMLException | NoSuchFieldException | IllegalAccessException e)
         {
             throw new ConfigurationException("Invalid yaml: " + url + SystemUtils.LINE_SEPARATOR
                                              +  " Error: " + e.getMessage(), false);
@@ -189,15 +195,84 @@ public class YamlConfigurationLoader implements ConfigurationLoader
 
         private final Set<String> nullProperties = new HashSet<>();
 
-        private static final Map<String, String> MAP_OLD_TO_NEW_PARAMETERS = new HashMap<String, String>()
+        private final Map<String, String> MAP_OLD_TO_NEW_PARAMETERS = new HashMap<String, String>()
         {
             {
+                put("max_hint_window_in_ms", "max_hint_window");
+                put("native_transport_idle_timeout_in_ms", "native_transport_idle_timeout");
+                put("request_timeout_in_ms", "request_timeout");
+                put("read_request_timeout_in_ms", "read_request_timeout_in_ms");
+                put("range_request_timeout_in_ms", "range_request_timeout");
+                put("write_request_timeout_in_ms", "write_request_timeout");
+                put("counter_write_request_timeout_in_ms", "counter_write_request_timeout");
+                put("cas_contention_timeout_in_ms", "cas_contention_timeout");
+                put("truncate_request_timeout_in_ms", "truncate_request_timeout");
+                put("streaming_keep_alive_period_in_secs", "streaming_keep_alive_period");
+                put("slow_query_log_timeout_in_ms", "slow_query_log_timeout");
+                put("internode_tcp_connect_timeout_in_ms", "internode_tcp_connect_timeout");
+                put("internode_tcp_user_timeout_in_ms", "internode_tcp_user_timeout");
+                put("commitlog_sync_batch_window_in_ms", "commitlog_sync_batch_window");
+                put("commitlog_sync_group_window_in_ms", "commitlog_sync_group_window");
+                put("commitlog_sync_period_in_ms", "commitlog_sync_period");
+                put("periodic_commitlog_sync_lag_block_in_ms", "periodic_commitlog_sync_lag_block");
+                put("cdc_free_space_check_interval_ms", "cdc_free_space_check_interval");
+                put("dynamic_snitch_update_interval_in_ms", "dynamic_snitch_update_interval");
+                put("dynamic_snitch_reset_interval_in_ms", "dynamic_snitch_reset_interval");
+                put("gc_log_threshold_in_ms", "gc_log_threshold");
+                put("hints_flush_period_in_ms", "hints_flush_period");
+                put("gc_warn_threshold_in_ms", "gc_warn_threshold");
+                put("tracetype_query_ttl_in_s", "tracetype_query_ttl");
+                put("tracetype_repair_ttl_in_s", "tracetype_repair_ttl");
+                put("permissions_validity_in_ms", "permissions_validity");
+                put("permissions_update_interval_in_ms", "permissions_update_interval");
+                put("roles_validity_in_ms", "roles_validity");
+                put("roles_update_interval_in_ms", "roles_update_interval");
+                put("credentials_validity_in_ms", "credentials_validity");
+                put("credentials_update_interval_in_ms", "credentials_update_interval");
+                put("index_summary_resize_interval_in_minutes", "index_summary_resize_interval");
+
+                put("max_hints_file_size_in_mb", "max_hints_file_size");
+                put("memtable_heap_space_in_mb", "memtable_heap_space");
+                put("memtable_offheap_space_in_mb", "memtable_offheap_space");
+                put("repair_session_space_in_mb", "repair_session_space");
+                put("internode_application_send_queue_capacity_in_bytes", "internode_application_send_queue_capacity");
+                put("internode_application_send_queue_reserve_endpoint_capacity_in_bytes", "internode_application_send_queue_reserve_endpoint_capacity");
+                put("internode_application_send_queue_reserve_global_capacity_in_bytes", "internode_application_send_queue_reserve_global_capacity");
+                put("internode_application_receive_queue_capacity_in_bytes", "internode_application_receive_queue_capacity");
+                put("internode_application_receive_queue_reserve_endpoint_capacity_in_bytes", "internode_application_receive_queue_reserve_endpoint_capacity");
+                put("internode_application_receive_queue_reserve_global_capacity_in_bytes", "internode_application_receive_queue_reserve_global_capacity");
+                put("max_native_transport_frame_size_in_mb", "max_native_transport_frame_size");
+                put("native_transport_frame_block_size_in_kb", "native_transport_frame_block_size");
+                put("max_value_size_in_mb", "max_value_size");
+                put("column_index_size_in_kb", "column_index_size");
+                put("column_index_cache_size_in_kb", "column_index_cache_size");
+                put("batch_size_warn_threshold_in_kb", "batch_size_warn_threshold");
+                put("batch_size_fail_threshold_in_kb", "batch_size_fail_threshold");
+                put("compaction_large_partition_warning_threshold_mb", "compaction_large_partition_warning_threshold");
+                put("commitlog_total_space_in_mb", "commitlog_total_space");
+                put("commitlog_segment_size_in_mb", "commitlog_segment_size");
+                put("max_mutation_size_in_kb", "max_mutation_size");
+                put("cdc_total_space_in_mb", "cdc_total_space");
+                put("hinted_handoff_throttle_in_kb", "hinted_handoff_throttle");
+                put("batchlog_replay_throttle_in_kb", "trickle_fsync_interval");
+                put("sstable_preemptive_open_interval_in_mb", "sstable_preemptive_open_interval");
+                put("counter_cache_size_in_mb", "counter_cache_size");
+                put("file_cache_size_in_mb", "file_cache_size");
+                put("index_summary_capacity_in_mb", "index_summary_capacity");
+                put("prepared_statements_cache_size_mb", "prepared_statements_cache_size");
+                put("key_cache_size_in_mb", "key_cache_size");
+                put("row_cache_size_in_mb", "row_cache_size");
+
+                put("compaction_throughput_mb_per_sec", "compaction_throughput");
+                put("stream_throughput_outbound_megabits_per_sec", "stream_throughput_outbound");
+                put("inter_dc_stream_throughput_outbound_megabits_per_sec", "inter_dc_stream_throughput_outbound");
+
                 put("enable_user_defined_functions", "user_defined_functions_enabled");
                 put("enable_scripted_user_defined_functions", "scripted_user_defined_functions_enabled");
                 put("cross_node_timeout", "internode_timeout");
                 put("native_transport_max_threads", "max_native_transport_threads");
                 put("native_transport_max_frame_size_in_mb", "max_native_transport_frame_size_in_mb");
-                put("native_transport_max_concurrent_connections", "max_native_transport_frame_size_in_mb");
+                put("native_transport_max_concurrent_connections", "max_native_transport_concurrent_connections_in_mb");
                 put("native_transport_max_concurrent_connections_per_ip", "max_native_transport_concurrent_connections_per_ip");
                 put("otc_coalescing_strategy", "outbound_connection_coalescing_strategy");
                 put("otc_coalescing_window_us", "outbound_connection_coalescing_window_us");
@@ -225,6 +300,7 @@ public class YamlConfigurationLoader implements ConfigurationLoader
                 if(MAP_OLD_TO_NEW_PARAMETERS.containsKey(name))
                 {
                     isOldYAML = true;
+                    logger.warn("{} parameter has a new name. For more information, please refer to NEWS.txt", name);
                 }
                 else
                 {
@@ -232,6 +308,7 @@ public class YamlConfigurationLoader implements ConfigurationLoader
                 }
 
                 //Backward compatibility in case the user is still using the old version of the storage config yaml file
+                //Could be used in cases when for some reason only a name of a parameter is changed
                 //CASSANDRA-15234
                 if(isOldYAML)
                 {
@@ -272,6 +349,7 @@ public class YamlConfigurationLoader implements ConfigurationLoader
                     {
                         nullProperties.add(getName());
                     }
+
                     result.set(object, value);
                 }
 
@@ -302,9 +380,10 @@ public class YamlConfigurationLoader implements ConfigurationLoader
             }
             if (isOldYAML)
             {
-                logger.info("You are using the old version of the Cassandra storage config YAML file." +
+                logger.warn("You are using the old version of the Cassandra storage config YAML file." +
                                                  "Cassandra still provides backward compatibility but it is " +
-                                                 "recommended to consider update.");
+                                                 "recommended to consider update. For a full list of changed names, " +
+                                                 "please refer to NEWS.txt");
             }
         }
     }
