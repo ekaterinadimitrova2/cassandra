b6cdba3f29a3f1d172a51e9cfd0fa343e56c3323
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2021-05-19 17:19:16 -0400

    DO NOT COMMIT, only for one-time test purpose

--- test/long/org/apache/cassandra/cql3/ViewComplexTest.java
+++ test/long/org/apache/cassandra/cql3/ViewComplexTest.java
@@ -47,6 +47,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
