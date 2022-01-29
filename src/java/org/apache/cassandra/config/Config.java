3b89a7f925d999026248bbdc45fe1ecc18d1059d
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-01-29 14:44:35 -0500

    To be on the safe side change  to 24 all streaming config that was converted from 200megabits/s to 25MiB/s

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -284,8 +284,8 @@ public class Config
     @Replaces(oldName = "inter_dc_stream_throughput_outbound_megabits_per_sec", converter = Converters.MEGABITS_TO_MEBIBYTES_PER_SECOND_DATA_RATE, deprecated = true)
     public volatile DataRateSpec inter_dc_stream_throughput_outbound = new DataRateSpec("24MiB/s");
 
-    public volatile DataRateSpec entire_sstable_stream_throughput_outbound = new DataRateSpec("25MiB/s");
-    public volatile DataRateSpec entire_sstable_inter_dc_stream_throughput_outbound = new DataRateSpec("25MiB/s");
+    public volatile DataRateSpec entire_sstable_stream_throughput_outbound = new DataRateSpec("24MiB/s");
+    public volatile DataRateSpec entire_sstable_inter_dc_stream_throughput_outbound = new DataRateSpec("24MiB/s");
 
     public String[] data_file_directories = new String[0];
 
