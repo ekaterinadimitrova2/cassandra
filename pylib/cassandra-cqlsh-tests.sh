862f4e26d7a00f132490f40ca57201acfddaab8c
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-03-10 18:29:17 -0500

    python3.8

--- pylib/cassandra-cqlsh-tests.sh
+++ pylib/cassandra-cqlsh-tests.sh
@@ -73,7 +73,7 @@ set -e # enable immediate exit if venv setup fails
 virtualenv --python=$PYTHON_VERSION venv
 source venv/bin/activate
 
-pip install -r ${CASSANDRA_DIR}/pylib/requirements.txt --verbose
+/usr/bin/python3.8 -m pip install -r ${CASSANDRA_DIR}/pylib/requirements.txt --verbose
 pip freeze
 
 echo $PATH
