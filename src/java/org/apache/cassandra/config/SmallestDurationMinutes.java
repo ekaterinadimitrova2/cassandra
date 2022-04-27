eee653c34c96d9a599b122bbdb2ae0202be96848
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-04-27 11:26:13 -0400

    a few more corrections plus handle Long.MAX_VALUE

--- src/java/org/apache/cassandra/config/SmallestDurationMinutes.java
+++ src/java/org/apache/cassandra/config/SmallestDurationMinutes.java
@@ -40,12 +40,12 @@ public final class SmallestDurationMinutes extends DurationSpec
     {
         super(value, TimeUnit.MINUTES);
 
-        if (!value.equals("null"))
+        if (value != null)
         {
             long minutes = toMinutes();
             if (minutes == Long.MAX_VALUE)
                 throw new ConfigurationException("Invalid duration: values must be less than " + Long.MAX_VALUE +
-                                                 "minutes, but it was " + minutes + "minutes");
+                                                 " minutes, but it was " + minutes + " minutes");
         }
     }
 
