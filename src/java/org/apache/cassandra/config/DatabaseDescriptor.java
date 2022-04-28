704e436e0415d0a41dd083fabe4b8db161255c00
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-04-28 08:28:55 -0400

    MILLIS_CUSTOM_DURATION guarded

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1373,7 +1373,7 @@ public class DatabaseDescriptor
         {
             try
             {
-                conf.permissions_update_interval = SmallestDurationMilliseconds.inMilliseconds(updateInterval);
+                conf.permissions_update_interval = IntSmallestDurationMilliseconds.inMilliseconds(updateInterval);
             }
             catch (ConfigurationException e)
             {
@@ -1437,7 +1437,7 @@ public class DatabaseDescriptor
         {
             try
             {
-                conf.roles_update_interval = SmallestDurationMilliseconds.inMilliseconds(interval);
+                conf.roles_update_interval = IntSmallestDurationMilliseconds.inMilliseconds(interval);
             }
             catch(ConfigurationException e)
             {
@@ -1481,7 +1481,7 @@ public class DatabaseDescriptor
         {
             try
             {
-                conf.credentials_update_interval = SmallestDurationMilliseconds.inMilliseconds(updateInterval);
+                conf.credentials_update_interval = IntSmallestDurationMilliseconds.inMilliseconds(updateInterval);
             }
             catch (ConfigurationException e)
             {
