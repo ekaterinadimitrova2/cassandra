abab79abcf71f275bd64e85ce42f50dfd50f4316
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-04-28 08:31:43 -0400

    SECONDS_DURATION guarded

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -3233,7 +3233,7 @@ public class DatabaseDescriptor
     @VisibleForTesting
     public static void setCacheLoadTimeout(int seconds)
     {
-        conf.cache_load_timeout = SmallestDurationSeconds.inSeconds(seconds);
+        conf.cache_load_timeout = IntSmallestDurationSeconds.inSeconds(seconds);
     }
 
     public static int getCounterCacheKeysToSave()
