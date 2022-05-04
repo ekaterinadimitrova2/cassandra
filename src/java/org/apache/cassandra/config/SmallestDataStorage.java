44235f05752ecc9102d6ede953164f6b330e064f
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-05-04 18:52:46 -0400

    DataStorage

--- src/java/org/apache/cassandra/config/SmallestDataStorage.java
+++ src/java/org/apache/cassandra/config/SmallestDataStorage.java
@@ -22,99 +22,182 @@ import java.util.function.LongSupplier;
 
 import org.apache.cassandra.exceptions.ConfigurationException;
 
+/**
+ * Wrapper class for Cassandra data storage configuration parameters which are internally represented in Unit bigger than byte. In order
+ * not to lose precision while converting to smaller units (until we migrate those parameters to use internally the smallest
+ * supported unit) we restrict those parameters to use only the smallest provided or larger units. (CASSANDRA-15234)
+ */
 public abstract class SmallestDataStorage extends DataStorageSpec
 {
-    protected SmallestDataStorage(String value, DataStorageUnit unit)
+    protected SmallestDataStorage(String value, DataStorageUnit smallestUnit, boolean isInt)
     {
-        super(value, unit);
+        super(value, smallestUnit);
+
+        validateValue(value, this::quantity, this.unit(), smallestUnit, isInt);
     }
 
-    protected SmallestDataStorage(long quantity, DataStorageUnit unit)
+    protected SmallestDataStorage(long quantity, DataStorageUnit unit, DataStorageUnit smallestUnit, boolean isInt)
     {
         super(quantity, unit);
+
+        validateValue(quantity, unit, smallestUnit, isInt);
     }
 
-    protected static void validateLong(String value, LongSupplier valueSupplier, DataStorageUnit unit)
+    private static void validateValue(String value, LongSupplier valueSupplier, DataStorageUnit unit, DataStorageUnit smallestUnit, boolean isInt)
     {
-        if (valueSupplier.getAsLong() == Long.MAX_VALUE)
-            throw new ConfigurationException("Invalid data storage: " + value + ". It shouldn't be more than" +
-                                             (Long.MAX_VALUE - 1) + " in " + unit.name().toLowerCase());
+        if (!isInt)
+            validateLong(value, valueSupplier, unit, smallestUnit);
+        else
+            validateInt(value, valueSupplier, unit, smallestUnit);
     }
 
-    protected static void validateInt(LongSupplier valueSupplier, DataStorageUnit unit)
+    private static void validateValue(long quantity, DataStorageUnit unit, DataStorageUnit smallestUnit, boolean isInt)
     {
-        validateInt(valueSupplier.getAsLong(), unit);
+        if (!isInt)
+            validateLong(quantity, unit, smallestUnit);
+        else
+            validateInt(quantity, unit, smallestUnit);
     }
 
-    protected static void validateInt(long value, DataStorageUnit unit)
+    private static void validateLong(String value, LongSupplier valueSupplier, DataStorageUnit unit, DataStorageUnit smallestUnit)
     {
-        if (value > Integer.MAX_VALUE)
-            throw new ConfigurationException("Invalid data storage: values must be less than " + Integer.MAX_VALUE +
-                                             " "  + unit.name().toLowerCase() + ", but it was " + value + " " + unit.name().toLowerCase());
+        if (smallestUnit.convert(valueSupplier.getAsLong(), unit) == Long.MAX_VALUE)
+            throw new ConfigurationException("Invalid data storage: " + value + ". It shouldn't be more than " +
+                                             (Long.MAX_VALUE - 1) + " in " + smallestUnit.name().toLowerCase());
+    }
+
+    private static void validateInt(String value, LongSupplier valueSupplier, DataStorageUnit unit, DataStorageUnit smallestUnit)
+    {
+        if (smallestUnit.convert(valueSupplier.getAsLong(), unit) > Integer.MAX_VALUE)
+            throw new ConfigurationException("Invalid data storage: " + value + ". It shouldn't be more than " +
+                                             Integer.MAX_VALUE + " in " + smallestUnit.name().toLowerCase());
+    }
+
+    private static void validateLong(long quantity, DataStorageUnit unit, DataStorageUnit smallestUnit)
+    {
+        if (smallestUnit.convert(quantity, unit) == Long.MAX_VALUE)
+            throw new ConfigurationException("Invalid data storage: " + quantity + " " + unit.name().toLowerCase() + ". It shouldn't be more than " +
+                                             (Long.MAX_VALUE - 1) + " in " + smallestUnit.name().toLowerCase());
+    }
+
+    private static void validateInt(long quantity, DataStorageUnit unit, DataStorageUnit smallestUnit)
+    {
+        if (smallestUnit.convert(quantity, unit) > Integer.MAX_VALUE)
+            throw new ConfigurationException("Invalid data storage: " + quantity + " " + unit.name().toLowerCase() + ". It shouldn't be more than " +
+                                             Integer.MAX_VALUE + " in " + smallestUnit.name().toLowerCase());
     }
 
     public static class Kibibytes extends SmallestDataStorage
     {
+        /**
+         * Creates a {@code SmallestDataStorage.Kibibytes} of the specified amount.
+         *
+         * @param value the data storage
+         *
+         */
         public Kibibytes(String value)
         {
-            super(value, DataStorageUnit.KIBIBYTES);
-            validateLong(value, this::toKibibytes, DataStorageUnit.KIBIBYTES);
+            super(value, DataStorageUnit.KIBIBYTES, false);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.Kibibytes} of the specified amount in the specified unit.
+         *
+         * @param quantity where quantity shouldn't be bigger than Long.MAX_VALUE - 1 in kibibytes
+         * @param unit in which the provided quantity is
+         */
         public Kibibytes(long quantity, DataStorageUnit unit)
         {
-            super(quantity, unit);
+            super(quantity, unit, DataStorageUnit.KIBIBYTES, false);
         }
     }
 
     public static class Mebibytes extends SmallestDataStorage
     {
+        /**
+         * Creates a {@code SmallestDataStorage.Mebibytes} of the specified amount.
+         *
+         * @param value the data storage
+         */
         public Mebibytes(String value)
         {
-            super(value, DataStorageUnit.MEBIBYTES);
-            validateLong(value, this::toMebibytes, DataStorageUnit.MEBIBYTES);
+            super(value, DataStorageUnit.MEBIBYTES, false);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.Mebibytes} of the specified amount in the specified unit.
+         *
+         * @param quantity where quantity shouldn't be bigger than Long.MAX_VALUE - 1 in mebibytes
+         * @param unit in which the provided quantity is
+         */
         public Mebibytes(long quantity, DataStorageUnit unit)
         {
-            super(quantity, unit);
+            super(quantity, unit, DataStorageUnit.MEBIBYTES, false);
         }
     }
 
     public static class IntBytes extends SmallestDataStorage
     {
+        /**
+         * Creates a {@code SmallestDataStorage.IntBytes} of the specified amount which shouldn't be bigger than {@code Integer.MAX_VALUE}
+         * in bytes
+         * @param value the data storage
+         */
         public IntBytes(String value)
         {
-            super(value, DataStorageUnit.BYTES);
-            validateInt(this::toBytes, DataStorageUnit.BYTES);
+            super(value, DataStorageUnit.BYTES, true);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.IntBytes} of the specified amount in the specified unit.
+         *
+         * @param quantity where quantity shouldn't be bigger than Integer.MAX_VALUE in bytes
+         * @param unit in which the provided quantity is
+         */
         public IntBytes(long quantity, DataStorageUnit unit)
         {
-            super(quantity, unit);
-            validateInt(this::toBytes, DataStorageUnit.BYTES);
+            super(quantity, unit, DataStorageUnit.BYTES, true);
         }
 
-        public IntBytes(long quantityInBytes)
+        /**
+         * Creates a {@code SmallestDataStorage.IntBytes} of the specified amount in bytes.
+         *
+         * @param quantityInB where quantityInB shouldn't be bigger than Integer.MAX_VALUE
+         */
+        public IntBytes(long quantityInB)
         {
-            this(quantityInBytes, DataStorageUnit.BYTES);
+            this(quantityInB, DataStorageUnit.BYTES);
         }
     }
 
     public static class IntKibibytes extends SmallestDataStorage
     {
+        /**
+         * Creates a {@code SmallestDataStorage.IntKibibytes} of the specified amount which shouldn't be bigger than {@code Integer.MAX_VALUE}
+         * in kibibytes
+         * @param value the data storage
+         */
         public IntKibibytes(String value)
         {
-            super(value, DataStorageUnit.KIBIBYTES);
-            validateInt(this::toKibibytes, DataStorageUnit.KIBIBYTES);
+            super(value, DataStorageUnit.KIBIBYTES, true);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.IntKibibytes} of the specified amount in the specified unit.
+         *
+         * @param quantity where quantity shouldn't be bigger than Integer.MAX_VALUE in kibibytes
+         * @param unit in which the provided quantity is
+         */
         public IntKibibytes(long quantity, DataStorageUnit unit)
         {
-            super(quantity, unit);
-            validateInt(this::toKibibytes, DataStorageUnit.KIBIBYTES);
+            super(quantity, unit, DataStorageUnit.KIBIBYTES, true);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.IntKibibytes} of the specified amount in kibibytes.
+         *
+         * @param quantityInKiB where quantityInKiB shouldn't be bigger than Integer.MAX_VALUE
+         */
         public IntKibibytes(long quantityInKiB)
         {
             this(quantityInKiB, DataStorageUnit.KIBIBYTES);
@@ -123,18 +206,32 @@ public abstract class SmallestDataStorage extends DataStorageSpec
 
     public static class IntMebibytes extends SmallestDataStorage
     {
+        /**
+         * Creates a {@code SmallestDataStorage.IntMebibytes} of the specified amount which shouldn't be bigger than {@code Integer.MAX_VALUE}
+         * in mebibytes
+         * @param value the data storage
+         */
         public IntMebibytes(String value)
         {
-            super(value, DataStorageUnit.MEBIBYTES);
-            validateInt(this::toMebibytes, DataStorageUnit.MEBIBYTES);
+            super(value, DataStorageUnit.MEBIBYTES, true);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.IntMebibytes} of the specified amount in the specified unit.
+         *
+         * @param quantity where quantity shouldn't be bigger than Integer.MAX_VALUE in mebibytes
+         * @param unit in which the provided quantity is
+         */
         public IntMebibytes(long quantity, DataStorageUnit unit)
         {
-            super(quantity, unit);
-            validateInt(this::toMebibytes, DataStorageUnit.MEBIBYTES);
+            super(quantity, unit, DataStorageUnit.MEBIBYTES, true);
         }
 
+        /**
+         * Creates a {@code SmallestDataStorage.IntMebibytes} of the specified amount in mebibytes.
+         *
+         * @param quantityInMiB where quantityInMiB shouldn't be bigger than Integer.MAX_VALUE
+         */
         public IntMebibytes(long quantityInMiB)
         {
             this(quantityInMiB, DataStorageUnit.MEBIBYTES);
