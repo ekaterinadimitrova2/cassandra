0ddfcc413dc7f814a691f11eca67e2f1fedc4263
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-11-01 11:06:39 -0400

    CircleCI: Add burn tests

--- .circleci/generate.sh
+++ .circleci/generate.sh
@@ -244,6 +244,7 @@ if (! (echo "$env_vars" | grep -q "REPEATED_UTESTS=" )); then
   delete_job "j11_unit_tests_repeat"
   delete_job "utests_compression_repeat"
   delete_job "utests_system_keyspace_directory_repeat"
+  delete_job "j8_burn_tests_repeat"
 fi
 if (! (echo "$env_vars" | grep -q "REPEATED_UTESTS_LONG=")); then
   delete_job "utests_long_repeat"
