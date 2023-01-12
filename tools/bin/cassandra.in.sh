106eda83cfe32f5cd4dc25dab98f0370a27a33b8
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2023-01-12 11:43:07 -0500

    ninja fix

--- tools/bin/cassandra.in.sh
+++ tools/bin/cassandra.in.sh
@@ -90,6 +90,7 @@ elif [ "$short" = "11" ] ; then
     JAVA_VERSION=11
 elif [ "$JVM_VERSION" \< "17" ] ; then
     echo "Cassandra 5.0 requires Java 11 or Java 17(or newer)."
+    exit 1;
 fi
 
 jvm=`echo "$java_ver_output" | grep -A 1 '[openjdk|java] version' | awk 'NR==2 {print $1}'`
