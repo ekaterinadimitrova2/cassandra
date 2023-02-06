1e31cc8f36b8af5b989d2792aefebec27b9be6dd
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2023-02-06 17:36:39 -0500

    fix modifiers

--- test/unit/org/apache/cassandra/transport/MessagePayloadTest.java
+++ test/unit/org/apache/cassandra/transport/MessagePayloadTest.java
@@ -85,8 +85,7 @@ public class MessagePayloadTest extends CQLTester
             return;
         try
         {
-            //Field modifiersField = Field.class.getDeclaredField("modifiers");
-            Field modifiersField = getModifiersField();
+            Field modifiersField = ReflectionUtils.getModifiersField();
             modifiersField.setAccessible(true);
             modifiersField.setInt(cqlQueryHandlerField, cqlQueryHandlerField.getModifiers() | Modifier.FINAL);
 
