690993e18ed9e84eae3546a4a32fe5a1dec09af9
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2023-02-13 21:17:57 -0500

    Start the scripts

--- tools/bin/cassandra.in.sh
+++ tools/bin/cassandra.in.sh
@@ -80,8 +80,9 @@ fi
 java_ver_output=`"${JAVA:-java}" -version 2>&1`
 jvmver=`echo "$java_ver_output" | grep '[openjdk|java] version' | awk -F'"' 'NR==1 {print $2}' | cut -d\- -f1`
 JVM_VERSION=${jvmver%_*}
+short=$(echo "${jvmver}" | cut -c1-2)
 
-JAVA_VERSION=11
+JAVA_VERSION=17
 if [ "$JVM_VERSION" = "1.8.0" ]  ; then
     JVM_PATCH_VERSION=${jvmver#*_}
     if [ "$JVM_VERSION" \< "1.8" ] || [ "$JVM_VERSION" \> "1.8.2" ] ; then
@@ -95,6 +96,11 @@ if [ "$JVM_VERSION" = "1.8.0" ]  ; then
     JAVA_VERSION=8
 elif [ "$JVM_VERSION" \< "11" ] ; then
     echo "Cassandra 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer)."
+    exit 1;
+elif [ "$short" = "11" ] ; then
+     JAVA_VERSION=11
+elif [ "$JVM_VERSION" \< "17" ] ; then
+    echo "Cassandra 5.0 requires Java 11 or Java 17(or newer)."
     exit 1;
 fi
 
