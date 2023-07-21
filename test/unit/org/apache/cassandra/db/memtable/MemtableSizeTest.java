4f8808b6c70d8bab9e3cb9121c73d82b98db044e
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2023-07-21 11:09:02 -0400

    Test is MemtableSizeTestBase was not running at all

--- test/unit/org/apache/cassandra/db/memtable/MemtableSizeTest.java
+++ test/unit/org/apache/cassandra/db/memtable/MemtableSizeTest.java
@@ -44,7 +44,7 @@ import org.github.jamm.MemoryMeter.Guess;
 // Note: This test can be run in idea with the allocation type configured in the test yaml and memtable using the
 // value memtableClass is initialized with.
 @RunWith(Parameterized.class)
-public abstract class MemtableSizeTestBase extends CQLTester
+public class MemtableSizeTest extends CQLTester
 {
     // Note: To see a printout of the usage for each object, add .printVisitedTree() here (most useful with smaller number of
     // partitions).
@@ -54,7 +54,7 @@ public abstract class MemtableSizeTestBase extends CQLTester
 //                                           .printVisitedTreeUpTo(1000)
                                            .build();
 
-    static final Logger logger = LoggerFactory.getLogger(MemtableSizeTestBase.class);
+    static final Logger logger = LoggerFactory.getLogger(MemtableSizeTest.class);
 
     static final int partitions = 50_000;
     static final int rowsPerPartition = 4;
