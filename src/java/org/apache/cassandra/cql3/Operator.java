7fcc367e035835973193dc1779c00287dfa2c734
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2024-08-19 21:39:38 -0400

    Fixes

--- src/java/org/apache/cassandra/cql3/Operator.java
+++ src/java/org/apache/cassandra/cql3/Operator.java
@@ -502,7 +502,7 @@ public enum Operator
         @Override
         public boolean requiresFilteringOrIndexingFor(ColumnMetadata.Kind columnKind)
         {
-            return !columnKind.isPrimaryKeyKind();
+            return columnKind != ColumnMetadata.Kind.CLUSTERING;
         }
 
         @Override
@@ -666,7 +666,7 @@ public enum Operator
         @Override
         public boolean requiresFilteringOrIndexingFor(ColumnMetadata.Kind columnKind)
         {
-            return !columnKind.isPrimaryKeyKind();
+            return columnKind != ColumnMetadata.Kind.CLUSTERING;
         }
 
         @Override
